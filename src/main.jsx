d580b0738bf2baa059200bd4e9e22c3cc8fde765
Paul Padamadan <paul.padamadan@programming.com>
2024-07-19 15:35:14 +0530

    Add contact component

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from 'react-router-dom';
 
 import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: 'contacts/:contactId',
+        element: <Contact />
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+    </React.StrictMode>,
 )
