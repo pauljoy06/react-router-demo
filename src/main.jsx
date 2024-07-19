0246abf5acf10002298e66b2b0a9646c846356a9
Paul Padamadan <paul.padamadan@programming.com>
2024-07-19 15:21:18 +0530

    Add error page

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
+
 import Root from './routes/root';
+import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
